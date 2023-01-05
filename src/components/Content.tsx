e59776e22bd0a72200428199ae016db156cea381
Vincent Nduri <ndurivo@gmail.com>
2023-01-05 10:49:44 +0300

    Creates table structure

---
+++ src/components/Content.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Content() {
+  return (
+   <>
+     <div className='container py-3'>
+        <h2>Employees List</h2>
+        <div>
+            <table>
+              <thead>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Contact</th>
+                <th>Actions</th>
+              </thead>
+              <tbody>
+                <tr></tr>
+              </tbody>
+            </table>
+        </div>
+     </div>
+   </>
+  )
+}
+
+export default Content;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Content from './Content';
 import Header from './Header';
 
 function Home() {
   return (
     <>
       <Header />
+      <Content />
     </>
   )
 }
