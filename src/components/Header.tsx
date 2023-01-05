742b668a96229e1e6eb14e1c77b35a031264ed16
Vincent Nduri <ndurivo@gmail.com>
2023-01-05 10:22:55 +0300

    Creates the header file & import to Home

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+         <div>
+        <center>
+        <article>
+            <h2 className='py-3' style={{backgroundColor: "black", color: "white", height: 80}}>React Crud Application</h2>
+        </article>
+        </center>
+    </div>
+    </>
+  )
+}
+
+export default Header
